e068e869b55af567b348ab298f5da0c3fa60e38f
miaotouy <66715028+miaotouy@users.noreply.github.com>
2024-09-03 15:26:53 +0800

    Delete .history directory

--- .history/__init___20240903102032.py
+++
@@ -1,8 +0,0 @@
-from nonebot import require
-require("nonebot_plugin_datastore")
-require("nonebot_plugin_userinfo")
-from nonebot_plugin_datastore import PluginData
-plugin_data = PluginData(__name__)
-from .db.models import init_models
-User, Group, GroupUser = init_models(plugin_data)
-from .db import database, user_info_service
--- .history/db/__init___20240903102129.py
+++
@@ -1,4 +0,0 @@
-# db/__init__.py
-from . import models
-from . import database
-from . import user_info_service
